8fdc300dc958004354fe18e1709de0a4f1463b3d
SeungCheol <ssc6839@gmail.com>
2021-07-16 20:26:41 +0900

    db init

--- models/index.js
+++ models/index.js
@@ -1,36 +1,26 @@
-'use strict';
+const Sequelize = require('sequelize')
+const env = process.env.NODE_ENV || 'development'
+const config = require('../config/config')[env]
+const User = require('./user')
+const Post = require('./post')
+const Hashtag = require('./hashtag')
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+const db = {}
+const sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+)
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+db.sequelize = sequelize
+db.User = User
+db.Post = Post
+db.Hashtag = Hashtag
 
-fs
-    .readdirSync(__dirname)
-    .filter(file => {
-      return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    .forEach(file => {
-      var model = sequelize['import'](path.join(__dirname, file));
-      db[model.name] = model;
-    });
+User.init(sequelize)
+Post.init(sequelize)
+Hashtag.init(sequelize)
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+User.associate(db)
+Post.associate(db)
+Hashtag.associate(db)
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = db
