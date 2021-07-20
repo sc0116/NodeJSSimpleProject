f64971840f0a078e3aba45a07bf83f8d53426be8
SeungCheol <ssc6839@gmail.com>
2021-07-20 16:47:48 +0900

    multer

--- routes/page.js
+++ routes/page.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const { Post, User, Hashtag } = require('../models')
 
 const router = express.Router()
 
 router.use((req, res, next) => {
-    res.locals.user = null
-    res.locals.followerCount = 0
-    res.locals.followingCount = 0
-    res.locals.followerIdList = []
+    res.locals.user = req.user
+    res.locals.followerCount = req.user ? req.user.Followers.length : 0
+    res.locals.followingCount = req.user ? req.user.Followings.length : 0
+    res.locals.followingIdList = req.user ? req.user.Followers.map(f => f.id) : []
     next()
 })
 
@@ -18,13 +19,44 @@ router.get('/join', (req, res) => {
     res.render('join', {title: '회원가입 - NodeBird'})
 })
 
-router.get('/', (req, res, next) => {
-    const twits = []
-    res.render('main', {
-        title: 'NodeBird',
-        twits,
-        user: req.user
-    })
+router.get('/', async (req, res, next) => {
+    try {
+        const posts = await Post.findAll({
+            include: {
+                model: User,
+                attributes: ['id', 'nick']
+            },
+            order: [['createdAt', 'DESC']]
+        })
+        res.render('main', {
+            title: 'NodeBird',
+            twits: posts
+        })
+    } catch (error) {
+        console.error(error)
+        next(error)
+    }
+})
+
+router.get('/hashtag', async (req, res, next) => {
+    const query = req.query.hashtag
+    if (!query) {
+        return res.redirect('/')
+    }
+    try {
+        const hashtag = await Hashtag.findOne({ where: { title: query } })
+        let posts = []
+        if (hashtag) {
+            posts = await hashtag.getPosts({ include: [{ model: User, attributes: ['id', 'nick'] }] })
+        }
+        return res.render('main', {
+            title: `${query} 검색결과 | NodeBird`,
+            twits: posts
+        })
+    } catch (error) {
+        console.error(error)
+        return next(error )
+    }
 })
 
 module.exports = router
